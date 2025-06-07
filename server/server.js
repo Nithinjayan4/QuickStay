679a63a5503806dbbc41d2ae7eb4c690faeacdb0
Nithin S J <nithinjayanjasla@gmail.com>
2025-06-07 19:25:43 +0530

    clerk webhooks added backend

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
-import  express from 'express';
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
 import cors from "cors";
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhook from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
-const app= express();
+const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Api is running');
+//Middleware
+app.use(express.json());
+// Clerk Middleware
+app.use(clerkMiddleware());
+
+//API to list to clerk webhooks
+app.use("/api/clerk",clerkWebhook)
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
 });
 
 const PORT = process.env.PORT || 3000;
